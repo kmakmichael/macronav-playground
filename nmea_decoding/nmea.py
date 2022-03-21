c3f09875ad9c4372d1e44aa31968fd538f8f112e
kmakmichael <m_kmak@u.pacific.edu>
2022-03-21 10:18:09 -0700

    improvement ig

--- nmea_decoding/nmea.py
+++ nmea_decoding/nmea.py
@@ -7,17 +7,36 @@ def track(lat, long):
     print(f'location: {lat}, {long}')
 
 
+def datapr(msg):
+    if isinstance(msg, tk.RMC):
+        print('RMC:')
+        print(f'\tLat / Long\t{msg.latitude} / {msg.longitude}')
+        print(f'\tMode: {msg.mode}')
+    if isinstance(msg, tk.GSA):
+        print('GSA: ')
+        print(f'\tPDOP: {msg.pdop}')
+        print(f'\tHDOP: {msg.hdop}')
+        print(f'\tVDOP: {msg.vdop}')
+    if isinstance(msg, tk.GGA):
+        print(f'GGA: ')
+        print(f'\tHDOP: {msg.horizontal_dil}')
+        print(f'\tFix: {msg.gps_qual}')
+        print(f'\tSats: {msg.num_sats}')
+        print(f'\tMode: {msg.mode}')
+        print(f'\tMode Type: {msg.mode_fix_type}')
+    if isinstance(msg, tk.VTG):
+        print(f'VTG: ')
+        print(f'\tHeading: {msg.true_track}{msg.true_track_sym}')
+        print(f'\tSpeed: {msg.spd_over_grnd_kmph}{msg.spd_over_grnd_kmph_sym}')
+        print(f'\tMode: {msg.faa_mode}')
+
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', baudrate=9600)
     while True:
         try:
             msg = pynmea2.parse(ser.readline().decode("ascii"))
-            print(msg)
-            if isinstance(msg, pynmea2.types.talker.RMC):
-                if msg.status == 'V':
-                    print('No fix, discarding message')
-                else:
-                    track(msg.latitude, msg.longitude)
+            datapr(msg)
         except serial.SerialException as e:
             print(f'Device error: {e}')
             break

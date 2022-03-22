7e4aeb625061e734e0c5487222ca736bdff2c6e6
AlchemyMouse <alchemymouse@gmail.com>
2022-03-22 10:53:59 -0700

    done with accuracy testing

--- acc_test/acc.py
+++ acc_test/acc.py
@@ -1,66 +1,56 @@
 import serial
 import pynmea2
 import pynmea2.types.talker as tk
-
-
-def track(lat, long):
-    print(f'location: {lat}, {long}')
-
-
-
-def datapr(msg):
-    if isinstance(msg, tk.RMC):
-        print('RMC:')
-        print(f'\tLat / Long\t{msg.latitude} / {msg.longitude}')
-        print(f'\tMode: {msg.mode}')
-    elif isinstance(msg, tk.GSA):
-        print('GSA:')
-        print(f'\tMode: {msg.mode}')
-        print(f'\tMode Type: {msg.mode_fix_type}')
-        print(f'\tPDOP: {msg.pdop}')
-        print(f'\tHDOP: {msg.hdop}')
-        print(f'\tVDOP: {msg.vdop}')
-    elif isinstance(msg, tk.GGA):
-        print('GGA:')
-        print(f'\tHDOP: {msg.horizontal_dil}')
-        print(f'\tFix: {msg.gps_qual}')
-        print(f'\tSats: {msg.num_sats}')
-    elif isinstance(msg, tk.VTG):
-        print('VTG:')
-        print(f'\tHeading: {msg.true_track}{msg.true_track_sym}')
-        print(f'\tSpeed: {msg.spd_over_grnd_kmph}{msg.spd_over_grnd_kmph_sym}')
-        print(f'\tMode: {msg.faa_mode}')
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', baudrate=9600)
     fix = False
     pos_data = ([], [])
-    dops = {
-        "pdop": [],
-        "hdop": [],
-        "vdop": []
-    }
+    dops = ([], [], [])
 
+    print('waiting for fix...')
     while True:
         try:
             msg = pynmea2.parse(ser.readline().decode("ascii"))
             if fix:
                 if isinstance(msg, tk.RMC):
-                    pos_data[0].append(msg.longitude)
-                    pos_data[1].append(msg.latitude)
+                    pos_data[0].append(float(msg.longitude))
+                    pos_data[1].append(float(msg.latitude))
                 elif isinstance(msg, tk.GSA):
-                    dops["pdop"].append(msg.pdop)
-                    dops["hdop"].append(msg.hdop)
-                    dops["vdop"].append(msg.vdop)
+                    dops[0].append(float(msg.pdop))
+                    dops[1].append(float(msg.hdop))
+                    dops[2].append(float(msg.vdop))
             else:
                 if isinstance(msg, tk.RMC):
-                    fix = (msg.status != 'V')
+                    if msg.status != 'V':
+                        fix = True
+                        print("got a fix")
         except serial.SerialException as e:
             print(f'Device error: {e}')
             break
         except pynmea2.ParseError as e:
             print(f'Parse error: {e}')
+        except KeyboardInterrupt as e:
+            break
+    plt.scatter(pos_data[0], pos_data[1])
+    plt.title('positional data')
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.show()
+
+    n, b, p = plt.hist(dops, 40, density=True, alpha=0.8)
+    plt.title(f'Distribution of DOPs')
+    plt.legend(['PDOP', 'HDOP', 'VDOP'])
+    plt.show()
+
+    t = range(0,len(dops[0]))
+    plt.plot(t, dops[0],'r', t, dops[1], 'b', t, dops[2], 'g')
+    plt.legend(['PDOP', 'HDOP', 'VDOP'])
+    plt.xlabel('Time')
+    plt.ylabel('DOPs')
+    plt.show()
 
 
 
